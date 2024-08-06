812b1ec82e72ee509cb3b76ba288ae839d6aa9bb
zor-cell <philippemanu.zorman@gmail.com>
2024-08-06 23:27:27 +0200

    added hand component

---
+++ frontend/src/app/components/hand/hand.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hand',
+  templateUrl: './hand.component.html',
+  styleUrl: './hand.component.css'
+})
+export class HandComponent {
+
+}
