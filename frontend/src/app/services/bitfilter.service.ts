808069ed69c13b13b06d2fa219034848ba72f803
zor-cell <philippemanu.zorman@gmail.com>
2024-08-07 02:09:31 +0200

    implemented services to minimize code duplication

--- frontend/src/app/services/bitfilter.service.ts
+++ frontend/src/app/services/bitfilter.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import {Color} from "../classes/color";
+import {Shape} from "../classes/shape";
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +8,23 @@ import { Injectable } from '@angular/core';
 export class BitfilterService {
 
   constructor() { }
+
+  setBitCount(n: Color | Shape) {
+    let count = 0;
+    while (n) {
+      count += n & 1;
+      n >>= 1;
+    }
+
+    return count;
+  }
+
+  isCompatible(color1: Color, shape1: Shape, color2: Color, shape2: Shape) {
+    //if the color is the same, the shape has to be different
+    let sameColorMissingShape = color1 === color2 && (shape1 & shape2) === 0;
+    //if the shape is the same, the color has to be different
+    let sameShapeMissingColor = shape1 === shape2 && (color1 & color2) === 0;
+
+    return sameColorMissingShape || sameShapeMissingColor;
+  }
 }
--- frontend/src/app/services/render.service.ts
+++ frontend/src/app/services/render.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import {CanvasPosition, Position, PositionMapper} from "../classes/position";
+import {Tile} from "../classes/tile";
+import {CellRenderOptions} from "../classes/options";
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +9,24 @@ import { Injectable } from '@angular/core';
 export class RenderService {
 
   constructor() { }
+
+  //draws a cell from a grid position
+  drawCellFromGridPos(ctx: CanvasRenderingContext2D, pos: Position, options: CellRenderOptions = {}) {
+    this.drawCell(ctx, PositionMapper.gridToCanvasPosition(pos), options);
+  }
+
+  //draws a cell from a canvas position
+  drawCell(ctx: CanvasRenderingContext2D, pos: CanvasPosition, options: CellRenderOptions = {}) {
+    if(options.clear) ctx.clearRect(pos.x, pos.y, Tile.SIZE, Tile.SIZE);
+    if(options.color) {
+      ctx.fillStyle = options.color;
+      ctx.strokeStyle = options.color;
+    }
+
+    ctx.beginPath();
+    ctx.rect(pos.x, pos.y, Tile.SIZE, Tile.SIZE);
+
+    if(options.fill) ctx.fill();
+    else ctx.stroke();
+  }
 }
