11f6227be90485c5024300babf038b7c80c4066e
zor-cell <philippemanu.zorman@gmail.com>
2024-08-08 20:22:45 +0200

    created event handler component

---
+++ frontend/src/app/components/event-handler/event-handler.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-event-handler',
+  templateUrl: './event-handler.component.html',
+  styleUrl: './event-handler.component.css'
+})
+export class EventHandlerComponent {
+
+}
